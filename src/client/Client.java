b3aca7067731fa891ccf20ccafa5c8b033f7f459
jose alejandro galvis nieto <jagn1997@hotmail.com>
2019-03-21 17:36:19 -0500

    ola k ase

---
+++ src/client/Client.java
@@ -0,0 +1,58 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+import server.Server;
+
+
+
+public class Client {
+	
+	private Ball ball;
+	
+	static Socket clientConnection; 
+	
+	public static void main(String[] args) {
+        
+	
+		DataInputStream in;
+		
+		DataOutputStream out;
+		
+		try {
+			clientConnection = new Socket(Server.IP_SERVER, Server.PORT);
+			
+			
+			
+			in = new DataInputStream(clientConnection.getInputStream());
+			out = new DataOutputStream(clientConnection.getOutputStream());
+			
+			out.writeUTF("hola");
+			
+			
+			clientConnection.close();
+			in.close();
+			out.close();
+			
+		} catch (Exception e) {
+			System.out.println("Se generó una excepcion");
+		} 
+
+	}
+	
+
+	public Ball getBall() {
+		return ball;
+	}
+
+	public void setBall(Ball ball) {
+		this.ball = ball;
+	}
+
+}
