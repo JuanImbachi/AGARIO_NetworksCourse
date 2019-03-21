d4b5dff75e19ad3eb0bbdfbd35b7fe1cac191ef2
Juan Imbachí <juan.imbachi@correo.icesi.edu.co>
2019-03-21 17:33:40 -0500

    Servidor básico para prueba

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,15 +1,48 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
-	public static String IP_SERVER="172.30.179.30";
-	
-	
+	public final static String IP_SERVER="172.30.179.30";
 	public static final int PORT = 36556;
+	
+	private static Socket socket;
+	
 	private static  ServerSocket serverSocket;
 	public Server() {
+		DataInputStream in;
+		DataOutputStream out;
+		
+		try {
+			serverSocket = new ServerSocket(PORT);
+			System.out.println("::Servidor en línea, esperando conexión::");
+			
+			while(true) {
+				
+				socket = serverSocket.accept();
+				
+				System.out.println("Conexión iniciada");
+				in = new DataInputStream(socket.getInputStream());
+				out = new DataOutputStream(socket.getOutputStream());
+				String mensajeObtenidoCliente = in.readUTF();
+				System.out.println("El mensaje enviado por el cliente fue : " + mensajeObtenidoCliente);
+				String respuestaServer ="En la bna prrito";
+				out.writeUTF(respuestaServer);
+				socket.close();
+				System.out.println("::Cliente desconectado, el mensaje encriptado ha sido enviado::");
+			
+			}
+	
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	
 		// TODO Auto-generated constructor stub
