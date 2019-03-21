69879cfa94a0cb2ffd7bce9fffed48ce542b0ec2
Juan Imbachí <juan.imbachi@correo.icesi.edu.co>
2019-03-21 17:16:57 -0500

    again

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,17 @@
 package server;
 
+import java.net.ServerSocket;
+
 public class Server {
 
+	public static String IP_SERVER="172.30.179.30";
+	
+	
+	public static final int PORT = 36556;
+	private static  ServerSocket serverSocket;
 	public Server() {
+		
+	
 		// TODO Auto-generated constructor stub
 	}
 
