76f0d21af35dfaf266c53895a61136e3a639380f
jagn1 <jagn1997@hotmail.com>
2019-03-24 13:27:11 -0500

    wr done

--- src/gui/Gui_WaitingRoom.java
+++ src/gui/Gui_WaitingRoom.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
@@ -16,91 +17,103 @@ import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.TitledBorder;
 
-public class Gui_WaitingRoom extends JDialog  {
+public class Gui_WaitingRoom extends JDialog {
 
-	private JLabel lblTitle,lblTimer,lblInfo;
+	private JLabel lblTitle, lblTimer, lblInfo;
 	private JList<Comparable> jlPlayers;
 	private DefaultListModel<Comparable> listModel;
 	
+	private ArrayList<String> playersInRoom;
+
 	private GUI_principal principal;
-	
+
 	public Gui_WaitingRoom(GUI_principal pri) {
-		principal=pri;
-		listModel = new DefaultListModel<Comparable>();
+		principal = pri;
 		
+		playersInRoom = new ArrayList<String>();
+		
+		listModel = new DefaultListModel<Comparable>();
+
 		setTitle("Icesi Games SA - AgarIO");
 		setLayout(new BorderLayout());
 		this.setSize(new Dimension(230, 300));
-		
+
 		lblTitle = new JLabel("\n      Agar.IO      \n");
 		lblTitle.setHorizontalAlignment(JLabel.CENTER);
 		Font font = new Font("Snap ITC", Font.PLAIN, 36);
 		lblTitle.setFont(font);
-		
+
 		lblTimer = new JLabel("00:00");
 		lblTimer.setHorizontalAlignment(JLabel.CENTER);
-		
+
 		lblInfo = new JLabel("La partida comenzará en: ");
 		lblInfo.setHorizontalAlignment(JLabel.CENTER);
-		
-		
+
 		jlPlayers = new JList<Comparable>();
-		
+
 		jlPlayers.setModel(listModel);
-		JScrollPane scrollPane = new JScrollPane(jlPlayers); 
-		scrollPane.setBounds(10,30,200,110); 
-		
-		
+		JScrollPane scrollPane = new JScrollPane(jlPlayers);
+		scrollPane.setBounds(10, 30, 200, 110);
+
 		jlPlayers.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		jlPlayers.setLayoutOrientation(JList.VERTICAL);
-		
+
 		JPanel p1 = new JPanel();
 		p1.setLayout(new BorderLayout());
-		TitledBorder border1 = BorderFactory.createTitledBorder("Players in room");
+		TitledBorder border1 = BorderFactory
+				.createTitledBorder("Players in room");
 		border1.setTitleColor(Color.BLUE);
 		p1.setBorder(border1);
-		p1.add(jlPlayers,BorderLayout.CENTER);
-		
-		JPanel p2= new JPanel();
+		p1.add(jlPlayers, BorderLayout.CENTER);
+
+		JPanel p2 = new JPanel();
 		p2.setLayout(new FlowLayout());
 		p2.add(lblTitle);
-		
-		JPanel p3= new JPanel();
+
+		JPanel p3 = new JPanel();
 		p3.setLayout(new FlowLayout());
-		p3.add(lblInfo); p3.add(lblTimer);
+		p3.add(lblInfo);
+		p3.add(lblTimer);
 
 		add(p2, BorderLayout.NORTH);
 		add(p1, BorderLayout.CENTER);
-		add(p3,BorderLayout.SOUTH);
+		add(p3, BorderLayout.SOUTH);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
-		
+
 	}
-	
-	
-	public void refresh(String[] data){
-		//int timeLeft = 120 - Integer.parseInt(data[0]);
-		//lblTimer.setText(timeLeft+"");
+
+	public void refresh(String[] data) {
 		lblTimer.setText(data[0]);
-//		jlPlayers.clearSelection();
-//		for(int i=data.length-1;i>0;i--){
-//			jlPlayers.add(new JLabel(data[i]));
-//		}
-		
-		
-		listModel.removeAllElements();
-		for(int i = 1; i<data.length; i++){
-		        listModel.addElement(data[i]);
+
+		ArrayList<String> newPlayers = new ArrayList<String>();
+
+		for (int i = 1; i < data.length; i++) {
+
+			if (!playersInRoom.contains(data[i])) {
+				newPlayers.add(data[i]);
+				playersInRoom.add(data[i]);
+			}
+
 		}
+
+		if (newPlayers.size() > 0) {
+
+			for (int i = 0; i < newPlayers.size(); i++) {
+
+				listModel.addElement(newPlayers.get(i));
+
+			}
+
+		}
+
 		jlPlayers.setModel(listModel);
 	}
-	
 
 	public void addPlayer(String nickname) {
 		listModel.addElement(nickname);
 	}
-	
+
 	public JLabel getLblTitle() {
 		return lblTitle;
 	}
@@ -114,26 +127,26 @@ public class Gui_WaitingRoom extends JDialog  {
 	}
 
 	public void setLblTimer(String time) {
-		int seconds=Integer.parseInt(time);
-		
-		int minutes= (int)seconds/60;
-		seconds = seconds%60;
-		String hora="";
-		
-		if(minutes<10) {
-			hora= "0"+minutes;
-		}else {
-			hora=minutes+"";
+		int seconds = Integer.parseInt(time);
+
+		int minutes = (int) seconds / 60;
+		seconds = seconds % 60;
+		String hora = "";
+
+		if (minutes < 10) {
+			hora = "0" + minutes;
+		} else {
+			hora = minutes + "";
 		}
-		
-		hora= hora+":";
-		
-		if(seconds<10) {
-			hora= hora+"0"+seconds;
-		}else {
-			hora= hora+seconds;
+
+		hora = hora + ":";
+
+		if (seconds < 10) {
+			hora = hora + "0" + seconds;
+		} else {
+			hora = hora + seconds;
 		}
-		
+
 		this.lblTimer.setText(hora);
 	}
 
@@ -161,5 +174,4 @@ public class Gui_WaitingRoom extends JDialog  {
 		this.listModel = listModel;
 	}
 
-	
 }
